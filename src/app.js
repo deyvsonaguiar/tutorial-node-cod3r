6f63f1552792d6297b643f3e5fc4d43a1f5bd1fc
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2023-04-29 19:36:24 -0400

    Teste Listagem de usuários

--- src/app.js
+++ src/app.js
@@ -7,4 +7,11 @@ app.get('/', (req, res) => {
   res.status(200).send();
 });
 
+app.get('/users', (req, res) => {
+  const user = [
+    { name: 'Deyvson', email: 'deyvsonaguiar@gmail.com' },
+  ];
+  res.status(200).json(user);
+});
+
 module.exports = app;
